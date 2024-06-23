12629cc81a3c44e5bdfc371c6e91f0d4deb9d60a
StarPlatinumHH <157041819+StarPlatinumHH@users.noreply.github.com>
2024-06-23 21:07:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-//   $('#menu').click(function(){
-//     $(this).toggleClass('fa-times');
-//     $('.navbar').toggleClass('nav-toggle');
-//   });
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
+  });
 
-//   $(window).on('scroll load',function(){
+  $(window).on('scroll load',function(){
 
-//     $('#menu').removeClass('fa-times');
-//     $('.navbar').removeClass('nav-toggle');
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
 
-//     if($(window).scrollTop() > 0){
-//       $('.scroll-top').show();
-//     }else{
-//       $('.scroll-top').hide();
-//     }
+    if($(window).scrollTop() > 0){
+      $('.scroll-top').show();
+    }else{
+      $('.scroll-top').hide();
+    }
 
     // scroll spy 
 
